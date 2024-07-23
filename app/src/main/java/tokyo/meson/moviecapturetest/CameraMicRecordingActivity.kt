7de97a736007a4397af9f8f520dd4ede1c24e722
edom18 <edo.m18@gmail.com>
2024-07-23 14:33:34 +0900

    fix: 新しい Android Studio だと、パーミッションチェックがないとワーニングが出ていたので追加

--- app/src/main/java/tokyo/meson/moviecapturetest/CameraMicRecordingActivity.kt
+++ app/src/main/java/tokyo/meson/moviecapturetest/CameraMicRecordingActivity.kt
@@ -1,5 +1,6 @@
 package tokyo.meson.moviecapturetest
 
+import android.Manifest
 import android.content.pm.PackageManager
 import android.media.*
 import android.os.Bundle
@@ -178,7 +179,22 @@ class CameraMicRecordingActivity : AppCompatActivity() {
 
     private fun startAudioRecording() {
         val bufferSize = AudioRecord.getMinBufferSize(SAMPLE_RATE, AudioFormat.CHANNEL_IN_MONO, AudioFormat.ENCODING_PCM_16BIT)
-        
+
+        if (ActivityCompat.checkSelfPermission(
+                this,
+                Manifest.permission.RECORD_AUDIO
+            ) != PackageManager.PERMISSION_GRANTED
+        ) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return
+        }
+
         audioRecord = AudioRecord(
             MediaRecorder.AudioSource.MIC,
             SAMPLE_RATE,
